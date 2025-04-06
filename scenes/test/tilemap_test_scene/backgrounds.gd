2c6aba848beaf697cbc878582db873dd3735c090
Nicolas Lanore <nicolas.lanore@gmail.com>
2025-04-06 12:35:07 +0200

    ajout icon

--- scenes/test/tilemap_test_scene/backgrounds.gd
+++ scenes/test/tilemap_test_scene/backgrounds.gd
@@ -4,4 +4,12 @@ extends Node
 
 func _ready() -> void:
 	# animation_player.play("to_day")
+	Phase.phase_changed.connect(on_phase_changed)
 	pass
+
+func on_phase_changed():
+	match Phase.current_phase:
+		"cinematic":
+			animation_player.play("to_day")
+		"drill":
+			animation_player.play("to_night")
